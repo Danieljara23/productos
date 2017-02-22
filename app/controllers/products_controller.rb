d89d44009b0232edbb3e75f7acaf75b02c4c5191
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-02-21 23:19:50 -0500

    Everything is already working perfect. Edit,Remove and Create actions are already working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-
+before_action :find_product, only: [:edit,:update,:destroy]
   def new
     @product = Product.new
   end
@@ -18,7 +18,26 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def update
+    if @product.update(product_params)
+      flash[:notice] = "Product was successfully updated"
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    flash[:notice] = "Product was successfully removed"
+    redirect_to products_path
+  end
+
   private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name,:price,category_ids:[])
